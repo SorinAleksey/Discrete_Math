d815a36bfae2b9b7c77b4f03f7719ebe75de638f
Danil Lamov <Danil_lamov@mail.ru>
2020-04-18 18:26:35 +0300

    Add files via upload
    Update

--- Discrete Math/Main.cpp
+++ Discrete Math/Main.cpp
@@ -6,11 +6,6 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "ru");
 	cout << "Discrete Math" << endl;
-	NN* a = readNN(), * b = readNN();
-	printNN(a);
-	printNN(b);
-	printNN(SUB_NDN_N(a, b, '2'));
-	//printNN(SUB_NN_N(a, b));
 
 	return 0;
 }
--- Discrete Math/NN.h
+++ Discrete Math/NN.h
@@ -13,15 +13,19 @@ int COM_NN_D(NN* n, NN* m);
 NN* ADD_NN_N(NN* n, NN* m);
 NN* MUL_Nk_N(NN* n, const unsigned int& k);
 NN* DIV_NN_Dk(NN* n, NN* m, const unsigned int& k);
+NN* GCF_NN_N(NN* n, NN* m);
 
 //Анастасия Данченкова
 void NZER_N_B(NN* n);
 NN* SUB_NN_N(NN* n, NN* m);
 NN* MUL_NN_N(NN* n, NN* m);
+NN* DIV_NN_N(NN* n, NN* m);
 
 //Юлия Веселкова
 NN* ADD_1N_N(NN* n);
 NN* MUL_ND_N(NN* n, const char& num);
 NN* SUB_NDN_N(NN* n, NN* m, const char& k);
+NN* MOD_NN_N(NN* n, NN* m);
+NN* LCM_NN_N(NN* n, NN* m);
 
 #endif
